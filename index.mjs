1d316deab43d630fc6cb83a2461fa5fb419c6d22
SmokyMcPotts <108499060+SmokyMcPotts@users.noreply.github.com>
2022-08-07 15:47:59 -0400

    Add files via upload

--- index.mjs
+++ index.mjs
@@ -1,55 +1,107 @@
-import {loadStdlib} from '@reach-sh/stdlib';
+import { loadStdlib, ask } from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
-const stdlib = loadStdlib(process.env);
+const stdlib = loadStdlib();
 
-console.log('Hello');
+const suStr = stdlib.standardUnit;
+const isNew = await ask.ask(
+    `Type Y for new game.\nType N to join an existing game.`,
+    ask.yesno
+);
+const who = isNew ? 'Alice' : 'Bob';
 
-const startingBalance = stdlib.parseCurrency(100);
-const [ accAlice, accBob ] =
-  await stdlib.newTestAccounts(2, startingBalance);
-console.log('Launching accounts...');
+console.log(`Starting backends, I will call you ${who}...`); 
 
-const ctcAlice = accAlice.contract(backend);
-const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
+let acc = null;
+const createAcc = await ask.ask(
+  `Would you like to create an account? (only possible on devnet)`,
+  ask.yesno
+);
+if (createAcc) {
+  acc = await stdlib.newTestAccount(stdlib.parseCurrency(1000));
+} else {
+  const secret = await ask.ask(
+    `What is your account secret?`,
+    (x => x)
+  );
+  acc = await stdlib.newAccountFromSecret(secret);
+};
 
-console.log('Starting backends...');
-console.log(`${ctcAlice.getInfo()}`);
+const fmt = (x) => stdlib.formatCurrency(x, 4);
+const getBal = async () => fmt(await stdlib.balanceOf(acc));
 
+console.log(`${createAcc ? 'Account created' : 'Account connected'}. Your current balance is ${await getBal()} ${suStr}.`);
+
+console.log(`Deploying contract...`);
+
+let ctc = null;
+if (isNew) {
+    const ctc = acc.contract(backend);
+    /*ctc.getInfo().then((info) => {
+        console.log(`The contract is deployed as = ${JSON.stringify(info)}`);
+    });*/
+    const info = await ctc.getInfo();
+    console.log(`Contract is deployed. Contract info is ${JSON.stringify(info)}`);
+} else {
+    const info = await ask.ask(`Please paste the contract info:`, JSON.parse);
+    const ctc = acc.contract(backend, info);
+};
+console.log(`You are attached to ${JSON.stringify(info)}`);
+
+console.log(`Opponent found. Morra game initiated. Choose wisely!`);
+const before = await getBal();
+console.log(`Your current balance is ${before} ${suStr}.`);
+
+const interact = {
+    ...stdlib.hasRandom,
+};
+
+const part = isNew ? ctc.p.Alice : ctc.p.Bob;
 const HAND = [0, 1, 2, 3, 4, 5];
 const GUESS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//const THROWN = [];
-//const rightAnswer = THROWN[0] + THROWN[1];
-const OUTCOME = ['Alice won', 'no one won', 'Bob won'];
+const WINNER = [`${isNew ? 'You win' : 'Bob wins'}.`, `Draw`, `${isNew ? 'Alice wins' : 'You win'}.`];
 
-const commonInteract = async (who) => ({
-  getHand: async () => {
-    const hand = Math.floor(Math.random() * 6);
-    console.log(`${who} threw ${HAND[hand]} fingers`);
-    THROWN.push(HAND[hand])
-    return hand;
-  },
-  getGuess: async (hand) => {
-    const guess = Math.floor(Math.random * (10 - HAND[hand])) + HAND[hand];
-    console.log(`${who} guessed ${GUESS[guess]}`)
-    return guess;
-  },
-  seeThrown: async (rightAnswer) => { 
-    console.log(`Total fingers thrown was ${rightAnswer}.`);
-  },
-  seeWinner: async (winner) => {
-    console.log(`${who} saw that ${OUTCOME[winner]}.`);
-  },
-});
+if (isNew) {
+    const bet = await ask.ask(`How much would you like to wager?`, (x) => stdlib.parseCurrency);
+    interact.wager = bet;
+    interact.daedline = { ETH: 50, ALGO: 100, CFX: 1000 }[stdlib.connector];
+} else {
+    interact.acceptWager = async (wager) => {
+        const accept = await ask.ask(`Do you accept the wager of ${fmt(wager)} ${suStr}?`, ask.yesno);
+        if (!accept) {
+            process.exit(0);
+        };
+    };
+};
 
-await Promise.all([
-  backend.Alice(ctcAlice, {
-    ...commonInteract('Alice'),
-    // implement Alice's interact object here
-  }),
-  backend.Bob(ctcBob, {
-    ...commonInteract('Bob'),
-    // implement Bob's interact object here
-  }),
-]);
+interact.getHand = async () => {
+    const hand = await ask.ask(`How many fingers will you throw?`, (x) => {
+        const hand = HAND[x];
+        if (!HAND.includes(hand)) {
+            throw Error(`Not a valid choice, please pick a number between 0 and 5.`);
+        }
+        return hand;
+    });
+    console.log(`You threw ${HAND[hand]} fingers.`);
+};
 
-console.log('Goodbye, Alice and Bob!');
+interact.getGuess = async () => {
+    const guess = await ask.ask(`How many total fingers will you guess?`, (x) => {
+        const guess = GUESS[x];
+        if (!GUESS.includes(guess)) {
+            throw Error(`Not a valid choice, please pick a number between 0 and 10.`);
+        }
+        return guess;
+    });
+    console.log(`You guessed ${GUESS(guess)} total fingers.`);
+};
+
+interact.seeWinner = async (winner) => {
+    console.log(`The outcome is ${WINNER[winner]}`);
+};
+
+await part(interact);
+
+const after = await getBal();
+console.log(`Your balance went from ${before} ${suStr} to ${after} ${suStr}.`);
+
+ask.done();
--- index.rsh
+++ index.rsh
@@ -1,40 +1,70 @@
 'reach 0.1';
 
 const commonInteract = {
+  ...hasRandom, 
   getHand: Fun([], UInt),
-  getGuess: Fun([UInt], UInt),
+  getGuess: Fun([], UInt),
   seeThrown: Fun([UInt], Null),
+  informTimeout: Fun([], Null),
   seeWinner: Fun([UInt], Null),
 };
 
 export const main = Reach.App(() => {
   const A = Participant('Alice', { 
     ...commonInteract,
+    wager: UInt,
+    deadline: UInt,
     // Specify Alice's interact interface here
   });
   const B = Participant('Bob', {
     ...commonInteract,
+    acceptWager: Fun([UInt], Null),
     // Specify Bob's interact interface here
   });
   init();
+
+  const informTimeout = () => {
+    each([A, B], () => {
+      interact.informTimeout();
+    });
+  };
   
   // The first one to publish deploys the contract
   A.only(() => {
-    const handAlice = declassify(interact.getHand());
-    const guessAlice = declassify(interact.getGuess(handAlice));
+    const wager = declassify(interact.wager);
+    const deadline = declassify(interact.deadline);
   });
-  A.publish( handAlice, guessAlice );
+  A.publish(wager, deadline)
+    .pay(wager);
+  commit();
+
+  B.only(() => {
+    interact.acceptWager(wager);
+  });
+  B.pay(wager)
+    .timeout(relativeTime(deadline), () => closeTo(A, informTimeout));
+  
+  //var winner = 1;
+  //invariant( balance() == 2 * wager );
+  //while ( winner == 1 ) {
+  commit(); //tab over below for while loop
+
+  A.only(() => {
+    const handAlice = declassify(interact.getHand());
+    const guessAlice = declassify(interact.getGuess());
+  });
+  A.publish(handAlice, guessAlice)
+    .timeout(relativeTime(deadline), () => closeTo(B, informTimeout));
   commit();
   
   // The second one to publish always attaches
   B.only(() => {
     const handBob = declassify(interact.getHand());
-    const guessBob = declassify(interact.getGuess(handBob));
+    const guessBob = declassify(interact.getGuess());
   });
   B.publish( handBob, guessBob );
-  commit();
+  //commit();
 
-  // write your program here
   const rightAnswer = (handAlice + handBob);
   const winner = () => {
     if ( guessAlice == guessBob ) {
@@ -47,11 +77,27 @@ export const main = Reach.App(() => {
       } else {
         return 1;
       };
-    };
-    each([A, B], () => {
-      nteract.seeWinner(winner);
-    });
-  };
+    }; //tab over above for while loop
+  };  // write your program here
+
+  const outcome = winner();
+  each([A, B], () => {
+    interact.seeThrown(rightAnswer);
+    interact.seeWinner(winner());
+  });
+  if (outcome == 0) {
+    transfer(2 * wager).to(A);
+    commit();
+  } else if (outcome == 2) {
+    transfer(2 * wager).to(B);
+    commit();
+  } else {
+    transfer(wager).to(A);
+    transfer(wager).to(B);
+    commit();
+  }
+
 
   exit();
-});
+  //}
+})
